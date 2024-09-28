dccd5d1c2a4eae45acb6ba51da7cae06c5727894
NIROB AHMED <127641238+UCA-COMMUNITY@users.noreply.github.com>
2024-09-28 22:12:50 +0600

    Update info2.js

--- scripts/commands/info2.js
+++ scripts/commands/info2.js
@@ -1,44 +1,46 @@
 module.exports.config = {
-    name: "admin",
-    version: "1.0.0",
-    permission: 0,
-    credits: "nayan",
-    prefix: true,
-    description: "",
-    category: "prefix",
-    usages: "",
-    cooldowns: 5,
-    dependencies: 
-	{
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
+  name: "admin",
+  version: "1.0.0",
+  permission: 0,
+  credits: "nayan",
+  prefix: true,
+  description: "",
+  category: "prefix",
+  usages: "",
+  cooldowns: 5,
+  dependencies: 
+{
+  "request":"",
+  "fs-extra":"",
+  "axios":""
+}
 };
 module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
 const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
+  hours = Math.floor(time / (60 * 60)),
+  minutes = Math.floor((time % (60 * 60)) / 60),
+  seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-  
-var callback = () => api.sendMessage({body:`𝗔𝗗𝗠𝗜𝗡 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡
-=======================
-❇️𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 :
-✢━ 𝗡𝗔𝗭𝗥𝗨𝗟 ━✢
-❇️𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝗡𝗔𝗢𝗚𝗔𝗢𝗡
-=======𝗖𝗢𝗡𝗧𝗔𝗖𝗧=======
-=======================
-❇️𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6
-❇ 𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗖𝗛𝗔𝗧 𝗟𝗜𝗡𝗞:
-https://m.me/ji.la.pi.6
-❇️𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: ✢ 𝗡𝗔𝗭𝗥𝗨𝗟 ✢
-❇️𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000350435589/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+
+var callback = () => api.sendMessage({body:`
+========================================
+𓆩♡𓆪 𝐍𝐚𝐦𝐞       : Rakib Chowdhury 
+𓆩♡𓆪 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : Rakib Chowdhury 
+𓆩♡𓆪 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
+𓆩♡𓆪 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Rajshahi
+𓆩♡𓆪 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
+𓆩♡𓆪 𝐀𝐠𝐞           : 𝟏𝟖+
+𓆩♡𓆪𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
+𓆩♡𓆪 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
+𓆩♡𓆪 𝐆𝐦𝐚𝐢𝐥       : fbking846@gmail.com
+𓆩♡𓆪 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801862260293
+𓆩♡𓆪 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/SYSTEM_ERROR_KING
+𓆩♡𓆪 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/100025013732141`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+  fs.unlinkSync(__dirname + "/cache/1.png"));  
+    return request(encodeURI(`https://graph.facebook.com/100025013732141/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-   };
+ };
