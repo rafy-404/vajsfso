bcd5b0b05ad8771558bae388a4408f6fa9ee6fa8
NIROB AHMED <127641238+UCA-COMMUNITY@users.noreply.github.com>
2024-09-28 23:30:34 +0600

    Update and rename usersid.js to uid2.js

--- scripts/commands/uid2.js
+++ scripts/commands/uid2.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "1.0.0",
 	permission: 0,
 	credits: "ryuko",
-	prefix: false,
+	prefix: 'awto',
 	description: "get user id.",
 	category: "without prefix",
 	cooldowns: 5
