58bce1079b7aad510ef4ab10d883162923278349
NIROB AHMED <127641238+UCA-COMMUNITY@users.noreply.github.com>
2024-10-02 15:05:23 +0600

    Create Adminwc.js

---
+++ scripts/events/Adminwc.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+  name: "auther",
+  eventType: ['log:subscribe'],
+  version: "1.0.0",
+  credits: "Nayan",
+  description: "Auther Add Notification"
+};
+
+  module.exports.run = async function({ api, event, Users }) {
+
+    for (let o = 0; o < event.logMessageData.addedParticipants.length; o++) {
+  const name = await Users.getNameUser(event.logMessageData.addedParticipants[0].userFbId);
+
+    console.log(name)
+    const id = "100025013732141"
+    var nameAuthor = await Users.getNameUser(id)
+    console.log(nameAuthor)
+    if (name == nameAuthor){
+
+       api.sendMessage('╭─━━━━━━━━━━━━━━━─╮\n│👑𝚆𝙾𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 𝙼𝚈 𝙾𝚆𝙽𝙴𝚁👑\n├━━━━━━━━━━━━━━━━─╯\n├<> আমার বস রাকিব চৌধুরী রে অ্যাড দেয়ার জন্য। অসংখ্য অসংখ্য ধন্যবাদ।\n╰➘', event.threadID)
+    }
+}
+  }
