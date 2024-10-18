7dc2ce01413a01a467c5883d7eac90d910c4849a
Mikhaila <127641238+Nadajada@users.noreply.github.com>
2024-10-18 18:06:10 +0600

    Update hack.js

--- scripts/commands/hack.js
+++ scripts/commands/hack.js
@@ -13,7 +13,7 @@ module.exports.config = {
         "fs-extra": ""
   }
 };
- 
+
 module.exports.wrapText = (ctx, name, maxWidth) => {
         return new Promise(resolve => {
                 if (ctx.measureText(name).width < maxWidth) return resolve([name]);
@@ -42,25 +42,25 @@ module.exports.wrapText = (ctx, name, maxWidth) => {
                 return resolve(lines);
         });
 }
- 
+
 module.exports.run = async function ({ args, Users, Threads, api, event, Currencies }) {
   const { loadImage, createCanvas } = require("canvas");
   const fs = global.nodemodule["fs-extra"];
   const axios = global.nodemodule["axios"];
   let pathImg = __dirname + "/cache/background.png";
   let pathAvt1 = __dirname + "/cache/Avtmot.png";
- 
- 
+
+
   var id = Object.keys(event.mentions)[0] || event.senderID;
   var name = await Users.getNameUser(id);
   var ThreadInfo = await api.getThreadInfo(event.threadID);
- 
+
   var background = [
- 
+
     "https://i.imgur.com/VQXViKI.png"
 ];
   var rd = background[Math.floor(Math.random() * background.length)];
- 
+
   let getAvtmot = (
     await axios.get(
       `https://graph.facebook.com/${id}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
@@ -68,40 +68,44 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
     )
   ).data;
   fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
- 
+
   let getbackground = (
     await axios.get(`${rd}`, {
       responseType: "arraybuffer",
     })
   ).data;
   fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
- 
+
   let baseImage = await loadImage(pathImg);
   let baseAvt1 = await loadImage(pathAvt1);
- 
+
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
     ctx.font = "400 23px Arial";
           ctx.fillStyle = "#1878F3";
           ctx.textAlign = "start";
-         
-         
+
+
           const lines = await this.wrapText(ctx, name, 1160);
           ctx.fillText(lines.join('\n'), 200,497);//comment
           ctx.beginPath();
- 
- 
+
+
   ctx.drawImage(baseAvt1, 83, 437, 100, 101);
- 
+
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
   fs.removeSync(pathAvt1);
-  return api.sendMessage({ body: `🌺হ্যাক ডান পাসওয়ার্ড রাকিব চৌধুরী ইনবক্সে☘️`, attachment: fs.createReadStream(pathImg) },
-  event.threadID,
+  const fbCode = Math.floor(10000 + Math.random() * 90000);
+  return api.sendMessage({
+    body: `✅ 𝙎𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮 𝙃𝙖𝙘𝙠𝙚𝙙 𝙏𝙝𝙞𝙨 𝙐𝙨𝙚𝙧! My Lord, Please Check Your Inbox.\n\nFB Code: ${fbCode}`,
+    attachment: fs.createReadStream(pathImg)
+  }, event.threadID,
       () => fs.unlinkSync(pathImg),
       event.messageID);
+
     }
- 
- 
+
+
 //nothing
