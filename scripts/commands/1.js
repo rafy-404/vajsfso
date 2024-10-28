4dbda456d496a12edf1849bd9cb7dbc52d3e96a2
Mikhaila <127641238+Nadajada@users.noreply.github.com>
2024-10-28 20:16:26 +0600

    Update 1.js

--- scripts/commands/1.js
+++ scripts/commands/1.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    name: "auther",
+    name: "a65",
     eventType: ['message'],
     version: "1.0.0",
     credits: "Nayan",
@@ -7,7 +7,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event }) {
-    const triggerEmoji = "✅"; // এই ইমোজি দিয়ে বার্তা শুরু হলে তা রিমুভ হবে
+    const triggerEmoji = "⚠️"; // এই ইমোজি দিয়ে বার্তা শুরু হলে তা রিমুভ হবে
 
     if (event.body && event.body.startsWith(triggerEmoji)) {
         // বার্তা রিমুভ করা হবে
