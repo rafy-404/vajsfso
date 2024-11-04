e5b43923c4710245ef9a2ccdcb065ebbadf67a61
Mikhaila <127641238+Nadajada@users.noreply.github.com>
2024-11-04 12:00:03 +0600

    Update murgi.js

--- scripts/commands/murgi.js
+++ scripts/commands/murgi.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "1.0.2",
 	permission: 2,
 	credits: "nayan",
-	prefix: ture,
+	prefix: fales,
 	description: "",
 	category: "without prefix",
 	usages: "[tag]",
