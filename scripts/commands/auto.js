e9054e07d3ff6489ace13809d7b7b5902d9eba12
Mikhaila <127641238+Nadajada@users.noreply.github.com>
2024-11-17 21:03:23 +0600

    Update auto.js

--- scripts/commands/auto.js
+++ scripts/commands/auto.js
@@ -17,7 +17,7 @@ handleEvent: async function ({ api, event, args }) {
     const fs = require("fs-extra")
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  const {alldown} = require("nayan-media-downloader")
+  const {alldown} = require("rakib-media-downloader")
   if (body.startsWith("https://")) {
   api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
 const data = await alldown(content);
@@ -36,40 +36,4 @@ const data = await alldown(content);
         }, event.threadID, event.messageID);
     }
 }
-}const body = content.toLowerCase();
-
-const {alldown} = require("nayan-media-downloader")
-
-if (body.startsWith("https://")) {
-
-api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
-
-const data = await alldown(content);
-
-console.log(data)
-
-const {low, high, title} = data.data;
-
-api.setMessageReaction("✔️", event.messageID, (err) => {}, true);
-
-const video = (await axios.get(high, {
-
-responseType: "arraybuffer",
-
-})).data;
-
-fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
-
-return api.sendMessage({
-
-body: `《TITLE》: ${title}`,
-
-attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
-
-}, event.threadID, event.messageID);
-
-}
-
-}
-
 }
